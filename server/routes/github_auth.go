e4b57bcb44e885255b643b68349818243317c95b
Mohamed Abdulla <mohabdulla@noreply.learn.reboot01.com>
2024-06-05 17:30:59 +0000

    Update 'server/routes/github_auth.go'

--- server/routes/github_auth.go
+++ server/routes/github_auth.go
@@ -88,13 +88,13 @@ func GithubCallback(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if existingUser != nil {
-		// Check if the existing user's auth type matches the current login platform
-		if existingUser.AuthType != "github" {
-			// Display an error message to the user
-			errorMessage := "Login failed. There is already an email registered with another platform."
-			http.Redirect(w, r, "/login?error="+url.QueryEscape(errorMessage), http.StatusTemporaryRedirect)
-			return
-		}
+		// // Check if the existing user's auth type matches the current login platform
+		// if existingUser.AuthType != "github" {
+		// 	// Display an error message to the user
+		// 	errorMessage := "Login failed. There is already an email registered with another platform."
+		// 	http.Redirect(w, r, "/login?error="+url.QueryEscape(errorMessage), http.StatusTemporaryRedirect)
+		// 	return
+		// }
 
 		// User already exists, perform login
 		err = CreateSession(w, r, models.UserRegisteration{
